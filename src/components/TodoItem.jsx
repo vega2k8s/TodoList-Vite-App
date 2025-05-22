296868b68169ff67097089d6818dadc9132193fa
vega2k8s <vega2k8s@gmail.com>
2025-05-22 11:15:45 +0900

    shouldComponentUpdate 오버라이드

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -3,6 +3,14 @@ import PropTypes from 'prop-types';
 import './TodoItem.css';
 
 class TodoItem extends Component {
+    /*
+        true(checked 변수에 변동이 있으면) 이면 render() 함수가 호출됨
+        false(checked 변수에 변동이 없으면) 이면 render() 함수가 호출되지 않음 (렌더링 생략)
+    */    
+    shouldComponentUpdate(nextProps, nextState) {
+        return this.props.checked !== nextProps.checked;
+    }
+
     render() {
         const { text, checked, id, onToggle, onRemove } = this.props;
         return (
--- src/components/TodoItemList.jsx
+++ src/components/TodoItemList.jsx
@@ -3,6 +3,14 @@ import PropTypes from 'prop-types';
 import TodoItem from './TodoItem';
 
 class TodoItemList extends Component {
+    /*
+        true(myTodos 변수에 변동이 있으면) 이면 render() 함수가 호출됨
+        false(myTodos 변수에 변동이 없으면) 이면 render() 함수가 호출되지 않음 (렌더링 생략)
+    */
+    shouldComponentUpdate(nextProps, nextState) {
+        return this.props.myTodos !== nextProps.myTodos;
+    }
+
     render() {
         const { myTodos, myToggle, myRemove } = this.props;
         const todoList = myTodos.map(
