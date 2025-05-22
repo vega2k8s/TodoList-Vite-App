c2680026eba0c1c99128e5a7bab83896d91fc787
vega2k8s <vega2k8s@gmail.com>
2025-05-22 09:56:46 +0900

    App => TodoItemList todos전달

--- src/components/TodoItemList.jsx
+++ src/components/TodoItemList.jsx
@@ -4,7 +4,7 @@ import TodoItem from './TodoItem';
 
 class TodoItemList extends Component {
     render() {
-        const { todos, myToggle, myRemove } = this.props;
+        const { myTodos, myToggle, myRemove } = this.props;
         return (
             <div>
                 <TodoItem text="오늘의 할일1" checked={false} />
@@ -15,7 +15,7 @@ class TodoItemList extends Component {
 }
 
 TodoItemList.propTypes = {
-    todos: PropTypes.array,
+    myTodos: PropTypes.array,
     myToggle: PropTypes.func,
     myRemove: PropTypes.func
 };
