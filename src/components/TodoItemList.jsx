c2efa4f867af468946ad2f6808f0e1d04a0d3d7b
vega2k8s <vega2k8s@gmail.com>
2025-05-23 11:22:58 +0900

    절대경로사용

--- src/components/TodoItemList.jsx
+++ src/components/TodoItemList.jsx
@@ -1,6 +1,6 @@
 import { Component } from 'react';
 import PropTypes from 'prop-types';
-import TodoItem from './TodoItem';
+import TodoItem from '@components/TodoItem';
 
 class TodoItemList extends Component {
     /*
