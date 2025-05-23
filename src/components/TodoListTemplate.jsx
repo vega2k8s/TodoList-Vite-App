baa04611be4289ebc7a1c48a6d949898eba1bf8c
vega2k8s <vega2k8s@gmail.com>
2025-05-23 10:33:15 +0900

    .env 변수 사용

--- src/components/TodoListTemplate.jsx
+++ src/components/TodoListTemplate.jsx
@@ -5,7 +5,7 @@ const TodoListTemplate = ({ form, children }) => {
     return (
         <main className="todo-list-template">
             <div className="title">
-                오늘의 할 일
+                오늘의 할 일 ({import.meta.env.VITE_MODE})
             </div>
             <section className="form-wrapper">
                 {form}
