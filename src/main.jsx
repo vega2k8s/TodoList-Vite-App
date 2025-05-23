ad8fd00067078cf8c374ee4fe73d957b23c97589
vega2k8s <vega2k8s@gmail.com>
2025-05-23 13:06:00 +0900

    configureStroe 추가

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,22 @@ import { createRoot } from 'react-dom/client'
 import '@/index.css'
 import App from '@/App.jsx'
 
+//import { createStore, applyMiddleware } from 'redux';
+//import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
+import { configureStore } from '@reduxjs/toolkit';
+import { toDoReducer } from './reducers';
+//const store = createStore(toDoReducer, applyMiddleware(thunk));
+
+const store = configureStore({
+  reducer: toDoReducer,
+  //middleware: getDefaultMiddleware => getDefaultMiddleware().concat(thunk)
+});
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </StrictMode>,
 )
