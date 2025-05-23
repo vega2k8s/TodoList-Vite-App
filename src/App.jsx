f1278afaa559115f862198600388f335a5603267
vega2k8s <vega2k8s@gmail.com>
2025-05-23 13:45:14 +0900

    Form 상태변수,핸들러함수 추가

--- src/App.jsx
+++ src/App.jsx
@@ -14,34 +14,8 @@ class App extends Component {
       { id: 2, text: '리액트 사용', checked: false }
     ]
   };
+
   //이벤트핸들러 함수 선언
-  handleChange = (e) => {
-    this.setState({
-      todo: e.target.value // input field의 다음 바뀔 값
-    });
-  }
-
-  handleCreate = () => {
-    const { todo, todos } = this.state;
-    const newTodo = {
-      id: this.id++,
-      text: todo,
-      checked: false
-    };
-
-    this.setState({
-      todos: [...todos, newTodo],
-      todo: '', // input 초기화
-    });
-  }
-
-  handleEnter = (e) => {
-    // 눌려진 키가 Enter Key 이면 handleCreate 호출
-    if (e.keyCode === 13) {
-      this.handleCreate();
-    }
-  };
-
   handleToggle = (id) => {
     const { todos } = this.state;
     this.setState({
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -3,6 +3,37 @@ import PropTypes from 'prop-types';
 import '@components/Form.css';
 
 class Form extends Component {
+    state = {
+        todo: ''
+    };
+
+    handleChange = (e) => {
+        this.setState({
+            todo: e.target.value // input field의 다음 바뀔 값
+        });
+    }
+
+    handleCreate = () => {
+        const { todo, todos } = this.state;
+        const newTodo = {
+            id: this.id++,
+            text: todo,
+            checked: false
+        };
+
+        this.setState({
+            todos: [...todos, newTodo],
+            todo: '', // input 초기화
+        });
+    }
+
+    handleEnter = (e) => {
+        // 눌려진 키가 Enter Key 이면 handleCreate 호출
+        if (e.keyCode === 13) {
+            this.handleCreate();
+        }
+    };
+
     render() {
         const { mytodo, myEnter, myChange, myCreate } = this.props;
         return (
